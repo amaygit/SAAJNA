4d01ab9a53d9912b507564a5898abf25e17df348
amaygit <mailamaynayak@gmail.com>
2025-07-06 23:56:45 +0530

    use connect is being set using tanstack query in provider

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,6 +1,49 @@
-const registerUser = async (req, res) => {};
+import User from '../models/user.js';
+import bcrypt from 'bcrypt';
+const registerUser = async (req, res) => {
 
-const loginUser = async (req, res) => {};
+    try {
+        // Extract user data from request body
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({email})
+
+        if(existingUser) {
+            return res.status(400).json({
+                message: "Email address already in use"
+            });
+        }
+        const salt= await bcrypt.genSalt(10);
+        const hashPassword= await bcrypt.hash(password, salt);
+        const newUser=await User.create({
+            name,
+            email,
+            password: hashPassword
+        });
+        //TODO: send verification email
+        res.status(201).json({
+            message: "Verifcation email sent to your eamil. Please check and verify your account",
+        });
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+
+};
+
+const loginUser = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.error("Error logging in user:", error);
+        res.status(500).json({
+            message: "Internal Server Error",
+        });
+        
+    }
+};
 
 export {
     registerUser,
--- backend/libs/validate-schema.js
+++ backend/libs/validate-schema.js
@@ -5,6 +5,11 @@ const registerSchema = z.object({
     password: z.string().min(8, "Password must be at least 8 characters long"),
 });
 
+const loginSchema = z.object({
+    email: z.string().email("Invlaid email address"),
+    password: z.string().min(1, "Password is required"),
+});
+
 export {
-    registerSchema
+    registerSchema,loginSchema
 }
